971a89b92edb002dfc7b720ff1a1c319547b1da5
ADITYA JHA <a4adityajha@gmail.com>
2023-04-19 00:38:23 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+// n;
+import Banner from "./images/Logo.ico";
+import NewCards from "./components/NewCards/NewCards";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { Toolbar, AppBar, Box, Typography } from "@material-ui/core";
+//copy alanApi key.
+const alanKey =
+  "60f28f55f1edd718116a162288f152ca2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
+  const [newsArticles, setnewsArticles] = useState([]);
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "NewHeadLines") {
+          setnewsArticles(articles);
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Box
+            component="img"
+            sx={{
+              height: 64,
+            }}
+            alt="Your logo."
+            src={Banner}
+            
+          />
+          <div>
+            <Typography variant ="h1">THE AI NEWS </Typography>
+          <br />
+            <Typography variant = "subtitle">Click on Mic Icon To Turn On Mic</Typography>
+            </div>
+        </Toolbar>
+      </AppBar>
+      <NewCards articles={newsArticles} />
     </div>
   );
 }
